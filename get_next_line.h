e2f9d5bd772180a84450c577aa94198c879d4b28
Yanael Barbier <b.yanael@gmail.com>
2014-11-10 18:27:44 +0100

    Send get_next_line files

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/10 18:05:40 by ybarbier          #+#    #+#             */
+/*   Updated: 2014/11/10 18:27:14 by ybarbier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+
+int	get_next_line(int const fd, char **line);
+
+#endif
