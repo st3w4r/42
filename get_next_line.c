4227aed490ac11da3ff5c528dbfbe6bfa0cd56b5
Yanael Barbier <b.yanael@gmail.com>
2014-11-13 17:26:19 +0100

    Update gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/10 18:05:23 by ybarbier          #+#    #+#             */
-/*   Updated: 2014/11/13 16:50:13 by ybarbier         ###   ########.fr       */
+/*   Updated: 2014/11/13 16:59:52 by ybarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static size_t		ft_len_c_stop(const char *str, int start, char c_stop)
 	return (counter);
 }
 
-static int ft_retln(char *str, char **line, char **save_buf)
+static int ft_retln(char *str, char **line, t_data *data)
 {
 	int len_str;
 
@@ -40,38 +40,37 @@ static int ft_retln(char *str, char **line, char **save_buf)
 	{
 		len_str = ft_len_c_stop(str, 0, '\n');
 		*line = ft_strjoin(*line, ft_strsub(str, 0, len_str));
-		*save_buf = ft_strsub(str, len_str + 1, ft_strlen(str) - len_str);
+		data->save_buf = ft_strsub(str, len_str + 1, ft_strlen(str) - len_str);
 		return (1);
 	}
 	return (0);
 }
 
-static int ft_read_line(int fd, char *buf, char **line)
+static int ft_read_line(int fd, char *buf, char **line, t_data *data)
 {
 	int ret;
-	static int is_read;
-	static char *save_buf;
 
-	if (!save_buf)
-		save_buf = ft_strdup("");
-	if (ft_retln(save_buf, line, &save_buf) == 1)
+	if (!(data->save_buf))
+		data->save_buf = ft_strdup("");
+	if (ft_retln(data->save_buf, line, data) == 1)
 	{
-		is_read = 1;
+		data->is_read = 1;
 		return (1);
 	}
-	save_buf = ft_strdup("");
+	data->save_buf = ft_strdup("");
 
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = 0;
-	
-		if (ft_retln(buf, line, &save_buf) == 1)
+		if (ft_retln(buf, line, data) == 1)
 			return (1);
-		is_read = 1;
+		data->is_read = 1;
 	}
-	if (ft_strcmp(save_buf, ""))
+	if (ret == -1)
+		return (-1);
+	if (data->is_read == 1)
 	{
-		is_read = 0;
+		data->is_read = 0;
 		return (1);
 	}
 	return (0);
@@ -80,10 +79,14 @@ static int ft_read_line(int fd, char *buf, char **line)
 int get_next_line(int const fd, char **line)
 {
 	char *buf;
+	static t_data *data;
 
+	if (!data)
+		if ((data = (t_data*)malloc(sizeof(t_data))) == NULL)
+			return (-1);
 	if (!(buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
 		return (-1);
 	buf[0] = 0;
 	*line = ft_strdup("");
-	return (ft_read_line(fd, buf, line));
+	return (ft_read_line(fd, buf, line, data));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/10 18:05:40 by ybarbier          #+#    #+#             */
-/*   Updated: 2014/11/12 19:33:40 by ybarbier         ###   ########.fr       */
+/*   Updated: 2014/11/13 17:18:02 by ybarbier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 3
+# define BUFF_SIZE 32
 
 
 #include <fcntl.h>
@@ -25,5 +25,11 @@
 # include "./libft/includes/libft.h"
 
 int	get_next_line(int const fd, char **line);
+
+typedef struct
+{
+   int is_read;
+   char *save_buf;
+}				t_data;
 
 #endif
